31febbbe02d65d645f6c9a0a67b756a396903b1b
Jeremy Ho <jujaga@gmail.com>
2024-12-11 14:15:23 -0800

    Add territorial acknowledgement to homepage
    There are some challenges for customizing the global footer via swizzling.
    However, we can at least insert in the land acknowledgement in the
    homepage to at least attempt to meet footer best practices on the landing.
    
    Signed-off-by: Jeremy Ho <jujaga@gmail.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -32,6 +32,23 @@ function HomepageHeader() {
   );
 }
 
+function HomepageFooter() {
+  return (
+    <div className="acknowledgement">
+      <div className="container">
+        <p className="row">
+          The B.C. Public Service acknowledges the territories of First Nations
+          around B.C. and is grateful to carry out our work on these lands. We
+          acknowledge the rights, interests, priorities, and concerns of all
+          Indigenous Peoples - First Nations, Métis, and Inuit - respecting and
+          acknowledging their distinct cultures, histories, rights, laws, and
+          governments.
+        </p>
+      </div>
+    </div>
+  );
+}
+
 export default function Home(): JSX.Element {
   const { siteConfig } = useDocusaurusContext();
   return (
@@ -43,6 +60,7 @@ export default function Home(): JSX.Element {
       <main>
         <HomepageFeatures />
       </main>
+      <HomepageFooter />
     </Layout>
   );
 }
