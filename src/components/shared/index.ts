8e5a8be611e97ae3cec0fda8e723d7790ba0dc68
Jeremy Ho <jujaga@gmail.com>
2024-12-18 17:51:40 -0800

    Graft over shared libs from jy95 to add support for resolving $ref
    Most of these TS files are grafted over verbatim from the
    jy95/docusaurus-json-schema-plugin repository as it provides a succinct
    way of acquiring and then rendering nested JSON schemas
    
    Signed-off-by: Jeremy Ho <jujaga@gmail.com>

---
+++ src/components/shared/index.ts
@@ -0,0 +1,4 @@
+export { default as generatePath } from './generatePath';
+export { default as generateResolverOptions } from './generateResolverOptions';
+export { default as localFileResolver } from './localFileResolver';
+export { default as remoteResolver } from './remoteResolver';
